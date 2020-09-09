91bbca19ce336d2f8266f9b7b5e98265e528a3b7
kamula <kamulaisaac@gmail.com>
2020-09-09 13:47:43 +0300

    post test

--- routes/users.js
+++ routes/users.js
@@ -14,12 +14,19 @@ client.connect((err, client) => {
         const db = client.db("symbiosis")
         console.log("connected to db")
         router.post("/register", (req, res) => {
+            let data = {}
+            data["firstname"] = req.body.firstname
+            data["lastname"] = req.body.lastname
+            data["email"] = req.body.email
+            data["datejoined"] = Date.now
             db.collection("users").insertOne({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                datejoined: Date.now
+                // firstname: req.body.firstname,
+                // lastname: req.body.lastname,
+                // email: req.body.email,
+                // datejoined: Date.now
+                data
             })
+            res.json(data)
         })
     }
 })
