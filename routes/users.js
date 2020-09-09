71f34a481b8af6738eaf70a0d6f5a5af69719319
kamula <kamulaisaac@gmail.com>
2020-09-09 16:25:04 +0300

    get and delete user requests

--- routes/users.js
+++ routes/users.js
@@ -14,19 +14,37 @@ client.connect((err, client) => {
         const db = client.db("symbiosis")
         console.log("connected to db")
         router.post("/register", (req, res) => {
-            let data = {}
-            data["firstname"] = req.body.firstname
-            data["lastname"] = req.body.lastname
-            data["email"] = req.body.email
-            data["datejoined"] = Date.now
-            db.collection("users").insertOne({
-                // firstname: req.body.firstname,
-                // lastname: req.body.lastname,
-                // email: req.body.email,
-                // datejoined: Date.now
-                data
+                let data = {
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    email: req.body.email,
+                    datejoined: new Date()
+                }
+
+                db.collection("users").insertOne({
+                    data
+                })
+                res.json(data)
             })
-            res.json(data)
+            //get all users user
+        router.get("/details", async(req, res) => {
+                await db.collection("users").find().toArray((err, result) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        res.json(result)
+                    }
+                })
+            })
+            //remove user
+        router.delete("/:userId", async(req, res) => {
+            try {
+                const deleteduser = await db.collection("users").remove({ _id: req.params.userId })
+                res.json(deleteduser)
+
+            } catch (err) {
+                throw err
+            }
         })
     }
 })
