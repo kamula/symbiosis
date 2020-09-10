29754b103bbd6d6445be13fdb0e3f6534b8545e7
kamula <kamulaisaac@gmail.com>
2020-09-10 08:37:41 +0300

    password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,63 @@
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient
-require("dotenv/config")
+const express = require('express');
+const bcrypt = require('bcrypt');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv/config');
 const router = express.Router();
 
-const url = process.env.USER_DB
+const url = process.env.USER_DB;
 
-const client = new MongoClient(url)
+const client = new MongoClient(url);
 
 client.connect((err, client) => {
     if (err) {
-        throw err
+        throw err;
     } else {
-        const db = client.db("symbiosis")
-        console.log("connected to db")
-        router.post("/register", (req, res) => {
-                let data = {
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    email: req.body.email,
-                    datejoined: new Date()
-                }
-
-                db.collection("users").insertOne({
-                    data
-                })
-                res.json(data)
-            })
-            //get all users user
-        router.get("/details", async(req, res) => {
-                await db.collection("users").find().toArray((err, result) => {
-                    if (err) {
-                        throw err
-                    } else {
-                        res.json(result)
-                    }
-                })
-            })
-            //remove user
-        router.delete("/:userId", async(req, res) => {
-            try {
-                const deleteduser = await db.collection("users").remove({ _id: req.params.userId })
-                res.json(deleteduser)
-
-            } catch (err) {
-                throw err
+        const db = client.db('symbiosis');
+        console.log('connected to db');
+        router.post('/register', (req, res) => {
+            let password = req.body.password;
+            let password2 = req.body.password2;
+            if (password === password2) {
+                bcrypt.hash(password, 10).then((hash) => {
+                    let data = {
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        email: req.body.email,
+                        datejoined: new Date(),
+                        password: hash,
+                    };
+                    db.collection('users').insertOne({
+                        data,
+                    });
+                    res.json(data);
+                });
             }
-        })
+            res.json({ message: 'passwords dont match' });
+        });
+        //get all users user
+        router.get('/details', async(req, res) => {
+            await db
+                .collection('users')
+                .find()
+                .toArray((err, result) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        res.json(result);
+                    }
+                });
+        });
+        //remove user
+        router.delete('/:userId', async(req, res) => {
+            try {
+                const deleteduser = await db
+                    .collection('users')
+                    .deleteOne({ _id: req.params.userId });
+                res.json(deleteduser);
+            } catch (err) {
+                throw err;
+            }
+        });
     }
-})
-module.exports = router
+});
+module.exports = router;
