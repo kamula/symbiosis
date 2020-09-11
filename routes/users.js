93728fbc0d4a6a10c11e0454271e6fe2aa1c865e
kamula <kamulaisaac@gmail.com>
2020-09-11 22:22:26 +0300

    modified user registration route

--- routes/users.js
+++ routes/users.js
@@ -16,52 +16,47 @@ client.connect((err, client) => {
         const db = client.db('symbiosis');
         console.log('connected to db');
         router.post('/register', async(req, res) => {
-            let password = req.body.password;
-            let password2 = req.body.password2;
-
-            if (password !== password2) {
-                res.json({ message: 'passwords dont match' });
-            } else {
-                bcrypt.hash(password, 10).then((hash) => {
-                    let data = {
-                        firstname: req.body.firstname,
-                        lastname: req.body.lastname,
-                        email: email,
-                        datejoined: new Date(),
-                        password: hash,
-                    };
-                    db.collection('users').insertOne({
-                        data,
-                    });
-                    sendmail(data['email']);
-                    res.json(data);
-                });
-            }
-        });
-        //get all users user
-        router.get('/details', async(req, res) => {
-            await db
-                .collection('users')
-                .find()
-                .toArray((err, result) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        res.json(result);
-                    }
-                });
-        });
-        //remove user
-        router.delete('/:userId', async(req, res) => {
-            try {
-                const deleteduser = await db
-                    .collection('users')
-                    .deleteOne({ _id: req.params.userId });
-                res.json(deleteduser);
-            } catch (err) {
-                throw err;
-            }
+            let data = {
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                datejoined: new Date(),
+                status: req.body.status,
+                password: await bcrypt.hash(req.body.password, 10),
+            };
+            db.collection('users').insertOne({
+                data,
+            });
+            sendmail(data['email']);
+            res.json(data);
         });
     }
 });
+//get all users user
+router.get('/details', async(req, res) => {
+    await db
+        .collection('users')
+        .find()
+        .toArray((err, result) => {
+            if (err) {
+                throw err;
+            } else {
+                res.json(result);
+            }
+        });
+});
+//remove user
+router.delete('/:userId', async(req, res) => {
+    try {
+        const deleteduser = await db
+            .collection('users')
+            .deleteOne({ _id: req.params.userId });
+        res.json(deleteduser);
+    } catch (err) {
+        throw err;
+    }
+});
+
+module.exports = router;
+
 module.exports = router;
