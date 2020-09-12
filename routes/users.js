65495320b93b2b885ec57003d460a4d237afa9c8
kamula <kamulaisaac@gmail.com>
2020-09-12 16:12:38 +0300

    login route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const MongoClient = require('mongodb').MongoClient;
+const jwt = require('jsonwebtoken');
 require('dotenv/config');
 const router = express.Router();
 const sendmail = require('./mail');
+const authuser = require('./authuser');
 
 const url = process.env.USER_DB;
 
@@ -16,47 +18,85 @@ client.connect((err, client) => {
         const db = client.db('symbiosis');
         console.log('connected to db');
         router.post('/register', async(req, res) => {
-            let data = {
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                datejoined: new Date(),
-                status: req.body.status,
-                password: await bcrypt.hash(req.body.password, 10),
-            };
-            db.collection('users').insertOne({
-                data,
-            });
-            sendmail(data['email']);
-            res.json(data);
+            db.collection('users')
+                .findOne({ email: req.body.email })
+                .then((user) => {
+                    if (!user) {
+                        bcrypt.hash(req.body.password, 10, (err, hash) => {
+                            hash = req.body.password;
+                            db.collection('users').insertOne({
+                                firstname: req.body.firstname,
+                                lastname: req.body.lastname,
+                                email: req.body.email,
+                                datejoined: new Date(),
+                                status: req.body.status,
+                                password: hash,
+                            });
+                            sendmail(req.body.email);
+                            res.json({
+                                message: 'data saved ',
+                            });
+                        });
+                    } else {
+                        res.json({ message: 'email exist' });
+                    }
+                });
         });
-    }
-});
-//get all users user
-router.get('/details', async(req, res) => {
-    await db
-        .collection('users')
-        .find()
-        .toArray((err, result) => {
-            if (err) {
+        //login  route
+        router.post('/login', async(req, res) => {
+            await db
+                .collection('users')
+                .findOne({ email: req.body.email })
+                .then((user) => {
+                    if (user) {
+                        if (bcrypt.compareSync(req.body.password, user.password)) {
+                            res.json({ message: 'logged in' });
+                            //redirect to new route
+                            //res.redirect("./mail")
+                            const loggeduser = {
+                                _id: user._id,
+                                firstname: user.firstname,
+                                lastname: user.lastname,
+                                email: user.email,
+                            };
+                            //let token = jwt.sign(loggeduser, { expiresIn: 1440 })
+                            console.log(loggeduser);
+                        } else {
+                            res.json({ message: 'passwords do not match' });
+                        }
+                    } else {
+                        res.json({ message: 'user does not exist' });
+                    }
+                })
+                .catch((err) => {
+                    res.json({ err: err });
+                });
+        });
+        //get all users user
+        router.get('/details', async(req, res) => {
+            await db
+                .collection('users')
+                .find()
+                .toArray((err, result) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        res.json(result);
+                    }
+                });
+        });
+        //remove user
+        router.delete('/:userId', async(req, res) => {
+            try {
+                const deleteduser = await db
+                    .collection('users')
+                    .deleteOne({ _id: req.params.userId });
+                res.json(deleteduser);
+            } catch (err) {
                 throw err;
-            } else {
-                res.json(result);
             }
         });
-});
-//remove user
-router.delete('/:userId', async(req, res) => {
-    try {
-        const deleteduser = await db
-            .collection('users')
-            .deleteOne({ _id: req.params.userId });
-        res.json(deleteduser);
-    } catch (err) {
-        throw err;
     }
 });
-
-module.exports = router;
 
 module.exports = router;
